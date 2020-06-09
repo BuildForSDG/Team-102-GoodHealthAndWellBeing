42c64ff514f05c0d3ff93872736725450f7d4633
Chukwuebuka J.O <53452638+happyjosh-tech@users.noreply.github.com>
2020-06-08 20:50:57 -0700

    This is the Blog Application for ZOIT Emergency accident response project

---
+++ src/mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail')
+
+]
